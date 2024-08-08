95ede6cb7662af55bad4ac8619a1c3c16f7bd9cd
Juan Carvajal <pcjvan@gmail.com>
2024-08-08 12:23:06 -0500

    add unions

--- the-beginnings/src/index.ts
+++ the-beginnings/src/index.ts
@@ -6,3 +6,23 @@ function log(message: string): void {
 
 const numbers: number[] = [1, 2, 3, 4, 5];
 
+// Type Union, one of ... | ...
+let myOtherName: null | string = null;
+
+console.log((myOtherName = 'hello'));
+
+const things: Array<string | number | boolean> = ['hello', 42];
+
+things.push(true);
+
+let result: string | number | boolean | undefined = things.pop();
+
+if (typeof result === 'string') {
+  result.split(' ');
+}
+
+const numStr: Array<string> = numbers.map((num: number): string => {
+  return num.toString();
+});
+
+console.log(numStr);
